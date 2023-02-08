fc4e4eb50ca9d02fb1657d8726566b45806dbd14
Anastasiia <schili@outlook.com>
2023-02-08 13:17:32 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
-const numberOfCategories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${numberOfCategories.length}`);
+const numberOfCategoriesEls = document.querySelectorAll(".item");
+console.log(`Number of categories: ${numberOfCategoriesEls.length}`);
 
-const itemsArray = [...numberOfCategories]
+const itemsArray = [...numberOfCategoriesEls]
   .map(
     (items) => `Category: ${items.children[0].textContent}
 Elements: ${items.children[1].children.length}`
